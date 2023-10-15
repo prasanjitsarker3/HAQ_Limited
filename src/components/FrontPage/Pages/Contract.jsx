54073ba7acb6254db8baf87206f62b22ea74c01b
Prasanjit Sarker <sarkerprasanjit379@gmail.com>
2023-10-15 22:27:04 +0600

    Code Front

--- src/components/FrontPage/Pages/Contract.jsx
+++ src/components/FrontPage/Pages/Contract.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaPhoneFlip } from "react-icons/fa6";
+import { FaPaperPlane, FaPhoneFlip, FaSeedling } from "react-icons/fa6";
 import { FaEnvelope, FaFacebookMessenger, FaMapMarkerAlt, FaTimes } from "react-icons/fa";
 import ContractSection from './FrontPage/ContractSection';
 import {
@@ -11,12 +11,20 @@ import {
     Input,
     Textarea,
     IconButton,
+    Typography,
 } from '@material-tailwind/react';
+import Accordion from './AccordionSection';
+import AccordionSection from './AccordionSection';
 
 const Contract = () => {
     const [open, setOpen] = useState(false);
 
     const handleOpen = () => setOpen(!open);
+    const [size, setSize] = React.useState(null);
+
+    const handleOpened = (value) => setSize(value);
+
+
     return (
         <div>
             <div>
@@ -75,9 +83,9 @@ const Contract = () => {
                         <div className="flex items-center justify-between">
                             <DialogHeader>New message to @</DialogHeader>
                             <IconButton onClick={handleOpen} variant="gradient" color='red' className="rounded-full mr-3">
-                                <FaTimes/>
+                                <FaTimes />
                             </IconButton>
-                           
+
                         </div>
                         <DialogBody divider>
                             <div className="grid gap-6">
@@ -99,11 +107,41 @@ const Contract = () => {
                     <img className=' h-12 w-12' src="https://cdn-icons-png.flaticon.com/128/4457/4457168.png" alt="" />
                     <h1 className='text-xl  uppercase font-semibold text-gray-800' >Live Chat</h1>
                     <h1 className=' text-lg flex-1 '>Chat with a member of our in-house team</h1>
-                    <Button color='blue' className='uppercase mt-auto flex  justify-center gap-1'>Start chat <FaFacebookMessenger /> </Button>
+                    <Button onClick={() => handleOpened("xs")} color='blue' className='uppercase mt-auto flex  justify-center gap-1'>Start chat <FaFacebookMessenger /> </Button>
+                    <Dialog
+                        open={
+                            size === "xs"
+                        }
+                        size={size || "md"}
+                        handler={handleOpened}
+                    >
+                        <div className=' flex justify-end'>
+                            <IconButton onClick={() => handleOpened(null)} variant="gradient" color='white' className="rounded-full mr-3">
+                                <FaTimes />
+                            </IconButton>
+                        </div>
+                        <DialogBody>
+                            <Typography className=' text-right'>Hello</Typography>
+                            <Typography className=' text-right'>How can i help you?</Typography>
+                            {/* <Typography className=' text-blue-600'>...</Typography> */}
+                            <div className="w-[60px] flex justify-start mr-5">
+                                <Input variant="static" placeholder='typing...' icon={<FaPaperPlane className='text-blue-600 cursor-pointer' />} />
+                            </div>
+                        </DialogBody>
+
+                    </Dialog>
                 </div>
             </div>
 
-            <ContractSection></ContractSection>
+            <div className='flex flex-col md:flex-row  gap-12 px-16 py-12'>
+                <div className='w-full md:w-1/2 '>
+                    <AccordionSection />
+                </div>
+                <div className='w-full md:w-1/2 '>
+                    <iframe className='w-full h-full' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d912.7471018198211!2d90.42534153979757!3d23.783427146331693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7e8b1ec3971%3A0x4eeaac0820774865!2sMisal%20International!5e0!3m2!1sen!2sbd!4v1697215687910!5m2!1sen!2sbd"
+                    ></iframe>
+                </div>
+            </div>
 
         </div>
     );
